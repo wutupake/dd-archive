8a2f1918a93ff7d272176ddf6a9bb2291d13fdbc
Mao <maurizio@Maurizios-MacBook-Pro.local>
2022-05-09 11:40:12 +0200

    wip add layout with static data

---
+++ pages/episodes/[slug].jsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { getAllSlug, getEpisodeBySlug } from "../../lib/episodes";
+
+export default function Episode(props) {
+  const { episodeData } = props;
+  return (
+    <article>
+      <Link href={'/'}>
+        <a>torna alla lista degli episodi</a>
+      </Link>
+      <h1>{episodeData.title}</h1>
+      <p>{episodeData.excerpt}</p>
+      {episodeData.tracklist}
+    </article>
+  )
+}
+
+export const getStaticPaths = () => {
+ const paths = getAllSlug();
+ return {
+   paths,
+   fallback: false
+ }
+}
+
+export const getStaticProps = ({ params }) => {
+  const episodeData = getEpisodeBySlug(params.slug);
+  return {
+    props: {
+      episodeData,
+    }
+  }
+}
